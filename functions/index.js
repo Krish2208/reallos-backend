4188b92f088b4fd8d0d21b41ec3b9bf271b7cfad
Krish2208 <diwankrish.17@gmail.com>
2020-07-03 21:46:53 +0530

    Added Basic Transaction CRUD

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,27 @@
-const functions = require('firebase-functions');
-const cors = require('cors');
+const functions = require("firebase-functions");
+const cors = require("cors");
 
-const app = require('express')();
-app.use(cors()); // using cors to for creating restful api 
+const app = require("express")();
+app.use(cors()); // using cors to for creating restful api
 
-
-const {signup,login} = require('./handlers/users');
-
-
+const { signup, login } = require("./handlers/users");
+const {
+  createTransaction,
+  deleteTransaction,
+  updateTransaction,
+  readTransaction,
+} = require("./handlers/transactions");
 
 //Signup Route
-app.post('/signup', signup);
+app.post("/signup", signup);
 
 // login route
-app.post('/login',login);
+app.post("/login", login);
 
+app.post("/create-transaction", createTransaction);
+app.post("/delete-transaction", deleteTransaction);
+app.post("/update-transaction", updateTransaction);
+app.post("/get-transaction", readTransaction)
+//app.post("/add-people", addPeople)
 
 exports.api = functions.https.onRequest(app); // Exporting the app
