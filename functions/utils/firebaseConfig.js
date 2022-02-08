2f03d813a083cb9ea72ce292edd69dcf9c36a750
Krish Agrawal <32320437+Krish2208@users.noreply.github.com>
2022-02-08 10:01:06 +0530

    Update firebaseConfig.js

--- functions/utils/firebaseConfig.js
+++ functions/utils/firebaseConfig.js
@@ -1,14 +1,14 @@
 const firebase = require('firebase');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBhN8rB3qXmFG_6TmVABtgLIMzLA-4-5yI",
-  authDomain: "reallos-382c7.firebaseapp.com",
-  databaseURL: "https://reallos-382c7.firebaseio.com",
-  projectId: "reallos-382c7",
-  storageBucket: "reallos-382c7.appspot.com",
-  messagingSenderId: "335659848409",
-  appId: "1:335659848409:web:e8c99b76c4c161af9724c8",
-  measurementId: "G-F60B6S0CXV"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 });
 
 module.exports = firebase;
